c57fb091cf1b9ce80c26c7177502c74eae03cc68
Sithila69 <kms123sithila@gmail.com>
2025-10-17 16:11:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import {Route, Routes,Link} from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/" element={<Dashboard/>}/>
+      {/* Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
